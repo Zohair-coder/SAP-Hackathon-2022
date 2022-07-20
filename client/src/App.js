29fba780ee153c59d6aa8ebebdf1cff46e3a96b6
systemc12ashe <system.c12ashe@gmail.com>
2022-07-20 10:08:48 -0400

    Log in screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 // import { useNavigate } from 'react-router-dom';
 import { ThemeProvider } from '@ui5/webcomponents-react';
-import LoginView from "./features/login";
-
+import LoginView from './features/login';
 
 function App() {
-  return <div>
-    <ThemeProvider>
-      <LoginView></LoginView>
-    </ThemeProvider>
-  </div>;
+  return (
+    <div>
+      <ThemeProvider>
+        <h1>The Missing Piece</h1>
+        <LoginView></LoginView>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
--- client/src/features/login.js
+++ client/src/features/login.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React from 'react';
 // import { useNavigate } from 'react-router-dom';
-import { Input, Label } from '@ui5/webcomponents-react';
 import {
-  FlexBox
+  Input,
+  Label,
+  FlexBoxAlignItems,
+  FlexBoxDirection,
+  FlexBoxJustifyContent,
+  Button,
 } from '@ui5/webcomponents-react';
+import { FlexBox } from '@ui5/webcomponents-react';
 
-function LoginView(){
-    return(
-        <div>
-            <FlexBox>
-                <Label>Username:</Label>
-                <Input></Input>
-            </FlexBox>
-        </div>
-    );
+function LoginView() {
+  return (
+    <div>
+      <FlexBox
+        direction={FlexBoxDirection.Column}
+        justifyContent={FlexBoxJustifyContent.Center}
+        alignItems={FlexBoxAlignItems.Start}
+      >
+        <Label>Username:</Label>
+        <Input></Input>
+        <Label>Password:</Label>
+        <Input></Input>
+        <h3>
+          Don't have an account?{' '}
+          <a href='https://github.com/Zohair-coder/SAP-Hackathon-2022'>
+            Sign Up
+          </a>
+        </h3>
+        <Button>Log In</Button>
+      </FlexBox>
+    </div>
+  );
 }
 
 export default LoginView;
