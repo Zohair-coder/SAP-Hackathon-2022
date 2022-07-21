02f5e2ac8a70d9aa27997ded3f972718902e3f4d
Cassidy Ashe <cassidy.ashe@sap.com>
2022-07-21 13:35:19 -0400

    profile changes

--- client/src/features/profile.js
+++ client/src/features/profile.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
-  Input,
-  Label,
   FlexBoxAlignItems,
   FlexBoxDirection,
   FlexBoxJustifyContent,
@@ -11,18 +9,35 @@ import {
 import { FlexBox } from '@ui5/webcomponents-react';
 
 function ProfileView() {
-    return (
+  return (
+    <div>
+      <h1> The Missing Piece</h1>
+      <FlexBox
+        direction={FlexBoxDirection.Column}
+        justifyContent={FlexBoxJustifyContent.Center}
+        alignItems={FlexBoxAlignItems.Start}
+      >
         <div>
-            <h1> The Missing Piece</h1>
-            <FlexBox
-                direction={FlexBoxDirection.Column}
-                justifyContent={FlexBoxJustifyContent.Center}
-                alignItems={FlexBoxAlignItems.Start}
-            >
-               
-            </FlexBox>
+          <img
+            src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
+            alt='profile'
+          ></img>
+          <h3>Username</h3>
+          <h3>Email</h3>
         </div>
-    )
+        <Link to={'/fridge'}>
+          <Button>My Fridge</Button>
+        </Link>
+        <Link to={'/'}>
+          <Button>Dietary Preferences</Button>
+        </Link>
+        <div>
+          <h3>Favorite Recipes</h3>
+          {/* Insert recipe container here */}
+        </div>
+      </FlexBox>
+    </div>
+  );
 }
 
 export default ProfileView;
